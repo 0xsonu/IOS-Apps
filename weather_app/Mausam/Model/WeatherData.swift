8d7609d40e58af76c1cd1e945bfc70a71e1f69e7
0xsonu <45717030+0xsonu@users.noreply.github.com>
2024-11-06 23:05:35 +0530

    Make network call

---
+++ weather_app/Mausam/Model/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDataModel.swift
+//  Mausam
+//
+//  Created by Sonu Kumar on 06/11/24.
+//  Copyright © 2024 0xsonu. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData {
+    
+}
--- weather_app/Mausam/Model/WeatherManager.swift
+++ weather_app/Mausam/Model/WeatherManager.swift
@@ -9,11 +9,50 @@
 import Foundation
 
 struct WeatherManager {
-    let apiUrl: String = "https://api.openweathermap.org/data/2.5/weather?appid=<API_KEY>&units=metric"
+    let apiUrl: String = "https://api.openweathermap.org/data/2.5/weather?appid=32893633ae5f0c821d8ddc7e4e83a687&units=metric"
     
     func fetchWeatherData(city: String){
         let url = "\(apiUrl)&q=\(city)"
-        
-        print(url)
+        print("Initiating Request on \(url)")
+        performRequest(urlString: url)
     }
+    
+    
+    // Network Call 4 step
+    func performRequest(urlString: String){
+        // 1. create URL
+        if let url = URL(string: urlString){
+            // 2. create URLSession
+            let session = URLSession(configuration: .default)
+            // 3. assign URLSession a task
+            let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data{
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            })
+            // 4. start the task
+            task.resume()
+        }
+        
+        
+    }
+    
+    // replaced by clouser
+//    func onCompletionHandler(data: Data?, response: URLResponse?, error: Error?){
+//        if error != nil{
+//            print(error!)
+//            return
+//        }
+//        
+//        if let safeData = data{
+//            let dataString = String(data: safeData, encoding: .utf8)
+//            print(dataString!)
+//        }
+//    }
 }
